5c59e75453d88d985955314d76884bdf6e726c09
Edi Piqoni <piqoni@gmail.com>
2025-09-28 20:48:05 +0200

    remove comments

--- app/app.go
+++ app/app.go
@@ -137,7 +137,6 @@ func (app *Application) postSystemMessage(message string) {
 	// Schedule the UI update to run on the main UI thread.
 	app.app.QueueUpdateDraw(func() {
 		systemMessage := fmt.Sprintf("\n System: %s", message)
-		// currentText := app.ui.GetChatText()
 		app.ui.AppendChatText(systemMessage)
 	})
 }
@@ -146,7 +145,6 @@ func (app *Application) SetMode(mode string) {
 	if app.Mode != mode {
 		app.Mode = mode
 		app.ui.SetMode(mode)
-		// app.postSystemMessage(fmt.Sprintf("Mode changed to: %s", mode))
 	}
 }
 
